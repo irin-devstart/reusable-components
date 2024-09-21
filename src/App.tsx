f5073f7c11db93896605b84e653b105dd92c455d
irin-devstart <irin.devstart@gmail.com>
2024-09-21 23:40:28 +0700

    Set app default

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import React from 'react'
-import TextField from './components/TextField'
+
 
 const App = () => {
   return (
-    <div className='flex justify-center  items-center flex-col gap-2'>
-      <TextField label='Name' />
-      <TextField label='Email' />
-      <TextField label='Email' />
-      <TextField label='Email' />
-
-    </div>
+    <div>App</div>
   )
 }
 
