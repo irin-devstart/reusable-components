be82f10af6c41a4cf1556118fdd14dc3a0cc8c43
irin-devstart <irin.devstart@gmail.com>
2024-09-30 00:53:56 +0700

    Set Default

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import React from "react";
-import Textarea from "./components/Textarea";
-
 const App = () => {
   return (
-    <div className="flex items-center flex-col w-[50vw] mt-6 gap-y-2">
-      <Textarea label="Address" />
-      <Textarea label="Description" required />
-      <Textarea label="Post" required helperText="Share someting...." />
-    </div>
+    <div className="flex items-center flex-col w-[60vw] mt-6 gap-y-2"></div>
   );
 };
 
