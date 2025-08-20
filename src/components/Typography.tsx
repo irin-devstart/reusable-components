a7ef86d4d348b6eda3da885b379c44c53bf71256
irin-devstart <irin.devstart@gmail.com>
2025-08-20 21:56:08 +0700

    update Typography component

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,33 +1,28 @@
-import clsx from 'clsx';
-import React from 'react'
+import clsx from "clsx";
+import React from "react";
 
-
-type TVariant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'p'
-
-type TSize = 'small' | 'medium' | 'large'
-
-
+type TVariant = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "p";
+type TSize = "small" | "medium" | "large";
 const sizes: Record<TSize, string> = {
-  large: 'text-[2em]',
-  medium: 'text-[1.5em]',
-  small: 'text-[1em]'
-}
-
-
+  large: "text-[2em]",
+  medium: "text-[1.5em]",
+  small: "text-[1em]",
+};
 
 interface TypographyProps {
-  variant?: TVariant
+  variant?: TVariant;
   className?: string;
-  children: React.ReactNode
-  size?: TSize
+  children: React.ReactNode;
+  size?: TSize;
 }
+const Typography = ({
+  children,
+  className,
+  variant = "h1",
+  size = "medium",
+}: TypographyProps) => {
+  const Element = variant;
+  return <Element className={clsx(className, sizes[size])}>{children}</Element>;
+};
 
-
-const Typography = ({ children, className, variant = 'h1', size = 'medium' }: TypographyProps) => {
-  const Element = variant
-  return (
-    <Element className={clsx(className, sizes[size])}>{children}</Element>
-  )
-}
-
-export default Typography
+export default Typography;
